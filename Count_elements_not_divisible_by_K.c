a6ac668ef2c5cfe34d8872fed48fbfff351af206
Sahityapennada <97424530+Sahityapennada@users.noreply.github.com>
2022-06-28 19:10:46 +0530

    #include<stdio.h> int main() {     int n,i,a[100],s=0,m;     scanf("%d%d",&n,&m);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%m!=0)             s++;     }     printf("%d",s); }

---
+++ Count_elements_not_divisible_by_K.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[100],s=0,m;
+    scanf("%d%d",&n,&m);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%m!=0)
+            s++;
+    }
+    printf("%d",s);
+}
